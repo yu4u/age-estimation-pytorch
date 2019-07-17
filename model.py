90294af09aba52cde3b8f300c3c7f5badc6bcd37
Yusuke Uchida <yusuke.a.uchida@dena.com>
2019-07-18 01:33:36 +0900

    accept arbitrary input size

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ def get_model(model_name="se_resnext50_32x4d", num_classes=101, pretrained="imag
     model = pretrainedmodels.__dict__[model_name](pretrained=pretrained)
     dim_feats = model.last_linear.in_features
     model.last_linear = nn.Linear(dim_feats, num_classes)
+    model.avg_pool = nn.AdaptiveAvgPool2d(1)
     return model
 
 
